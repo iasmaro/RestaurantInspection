b8bdd1390550ef71c951325c2aed269d9c6201ec
Riya Dhariwal <rdhariwa@sfu.ca>
2020-03-02 10:55:32 -0800

    Finished Restaurant class, with private fields and constructor.

---
+++ app/src/main/java/com/carbon/restaurantinspection/model/Restaurant.java
@@ -0,0 +1,59 @@
+package com.carbon.restaurantinspection.model;
+
+public class Restaurant {
+
+    private String trackingNumber;
+    private String name;
+    private String physicalAddress;
+    private String city;
+    private String factype;
+    private double latitude;
+    private double longitude;
+
+    public Restaurant(String trackingNumber, String name, String physicalAddress, String city, String factype,
+                      double latitude, double longitude) {
+        this.trackingNumber = trackingNumber;
+        this.name = name;
+        this.physicalAddress = physicalAddress;
+        this.city = city;
+        this.factype = factype;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhysicalAddress() {
+        return physicalAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getFactype() {
+        return factype;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "trackingNumber='" + trackingNumber + '\'' +
+                '}';
+    }
+}
+
