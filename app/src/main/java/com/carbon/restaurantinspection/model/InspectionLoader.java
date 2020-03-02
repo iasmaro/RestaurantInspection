60e129b8bd7d289dca571e49499a31a8890c5a6f
Ivan <ivan_asmaro@sfu.ca>
2020-03-01 20:38:12 -0800

    Create raw Directory And Add CSV File.
    Create raw directory to store csv files. Add inspection reports csv file
    into directory. The file contains information about the inspection
    reports. It will be read by the InspectionLoader class.

--- app/src/main/java/com/carbon/restaurantinspection/model/InspectionLoader.java
+++ app/src/main/java/com/carbon/restaurantinspection/model/InspectionLoader.java
@@ -1,4 +1,10 @@
 package com.carbon.restaurantinspection.model;
 
+import java.util.Hashtable;
+
 public class InspectionLoader {
+    public Hashtable<String, String> loadInspectionDetailList() {
+        Hashtable<String, String> loaded = new Hashtable<>();
+        return loaded;
+    }
 }
