1b397bf2b6e8162f210a05777e0972abb944d8f9
iasmaro <ivan_asmaro@sfu.ca>
2020-03-02 17:42:42 -0800

    Update InspectionDetail Class.
    Add inspectionType variable and its getter and setter.

--- app/src/main/java/com/carbon/restaurantinspection/model/InspectionDetail.java
+++ app/src/main/java/com/carbon/restaurantinspection/model/InspectionDetail.java
@@ -2,20 +2,22 @@ package com.carbon.restaurantinspection.model;
 
 /*
   InspectionDetail class models an inspection report's details.
-  It contains the inspection's date, tracking number, number of critical issues found,
+  It contains the inspection's tracking number, date, type, number of critical issues found,
   number of non-critical issues found, hazard level, and a summary of all the violations.
  */
 public class InspectionDetail {
     private String trackingNumber;
     private String inspectionDate;
+    private String inspectionType;
     private int numCritical;
     private int numNonCritical;
     private String hazardLevel;
     private String violations;
 
-    public InspectionDetail(String trackingNumber, String inspectionDate, int numCritical, int numNonCritical, String hazardLevel, String violations) {
+    public InspectionDetail(String trackingNumber, String inspectionDate, String inspectionType, int numCritical, int numNonCritical, String hazardLevel, String violations) {
         this.trackingNumber = trackingNumber;
         this.inspectionDate = inspectionDate;
+        this.inspectionType = inspectionType;
         this.numCritical = numCritical;
         this.numNonCritical = numNonCritical;
         this.hazardLevel = hazardLevel;
@@ -36,6 +38,14 @@ public class InspectionDetail {
 
     public void setInspectionDate(String inspectionDate) {
         this.inspectionDate = inspectionDate;
+    }
+    
+    public String getInspectionType() {
+        return inspectionType;
+    }
+
+    public void setInspectionType(String inspectionType) {
+        this.inspectionType = inspectionType;
     }
 
     public int getNumCritical() {
