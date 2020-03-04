1e0f85f03f3f62186de0e0c9f6598795714eb65a
Riya Dhariwal <rdhariwa@sfu.ca>
2020-03-04 00:09:22 -0800

    Changed colors.xml theme and added the background image.

---
+++ app/src/main/java/com/carbon/restaurantinspection/InspectionDetail.java
@@ -0,0 +1,14 @@
+package com.carbon.restaurantinspection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InspectionDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspection_detail);
+    }
+}
