7839d4ca90a178b5401064f9b33a0c258a8f3dca
Ivan <ivan_asmaro@sfu.ca>
2020-03-08 13:31:26 -0700

    Add Date And Full Date Methods.
    Change getInspectionDate method to return a string of "x days ago." if inspection date within last 30 days, a string of "month day" if inspection date within last year, a string of "month year" if inspection date more than one year ago. Add getFullDate method to return string of "month day, year" for the inspection date.

--- app/src/main/java/com/carbon/restaurantinspection/model/InspectionDetail.java
+++ app/src/main/java/com/carbon/restaurantinspection/model/InspectionDetail.java
@@ -1,5 +1,14 @@
 package com.carbon.restaurantinspection.model;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Hashtable;
+import java.util.Locale;
+
 /*
   InspectionDetail class models an inspection report's details.
   It contains the inspection's tracking number, date, type, number of critical issues found,
@@ -13,6 +22,7 @@ public class InspectionDetail {
     private int numNonCritical;
     private String hazardLevel;
     private String[] violations;
+    private final int MILISEC_TO_DAYS = 1000*3600*24;
 
     public InspectionDetail(String trackingNumber, String inspectionDate, String inspectionType, 
     int numCritical, int numNonCritical, String hazardLevel, String[] violations) {
@@ -34,11 +44,63 @@ public class InspectionDetail {
     }
 
     public String getInspectionDate() {
-        return inspectionDate;
+        Date date = getDate();
+        Calendar inspecDate = new GregorianCalendar();
+        inspecDate.setTime(date);
+        Calendar monthAgo = new GregorianCalendar();
+        monthAgo.add(monthAgo.DAY_OF_MONTH, -30);
+        Calendar yearAgo = new GregorianCalendar();
+        yearAgo.add(yearAgo.YEAR, -1);
+        String dateOfInspection = "";
+        if (monthAgo.before(inspecDate)) {
+            Calendar today = Calendar.getInstance();
+            long milliSecs = today.getTimeInMillis() - inspecDate.getTimeInMillis();
+            int days = (int) (milliSecs / MILISEC_TO_DAYS);
+            dateOfInspection = days + " days ago.";
+        }
+        else if (yearAgo.before(inspecDate)) {
+            String month = inspecDate.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.CANADA);
+            dateOfInspection = month + " " + inspecDate.DAY_OF_MONTH;
+        }
+        else {
+            String month = inspecDate.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.CANADA);
+            int year = inspecDate.YEAR;
+            dateOfInspection = month + " " + year;
+        }
+        return dateOfInspection;
+    }
+
+    public String getFullDate() {
+        Hashtable<String, String> months = new Hashtable<>();
+        months.put("01", "January");
+        months.put("02", "February");
+        months.put("03", "March");
+        months.put("04", "April");
+        months.put("05", "May");
+        months.put("06", "June");
+        months.put("07", "July");
+        months.put("08", "August");
+        months.put("09", "September");
+        months.put("10", "October");
+        months.put("11", "November");
+        months.put("12", "December");
+        String fullDate = months.get(inspectionDate.substring(4, 6)) + " "
+                + inspectionDate.substring(6) + ", "
+                + inspectionDate.substring(0, 4);
+        return fullDate;
     }
 
     public void setInspectionDate(String inspectionDate) {
         this.inspectionDate = inspectionDate;
+    }
+
+    private Date getDate(){
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+            return dateFormat.parse(inspectionDate);
+        } catch (ParseException e) {
+            return new Date();
+        }
     }
 
     public String getInspectionType() {
