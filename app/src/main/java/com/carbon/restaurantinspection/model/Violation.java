6fefb15998265a9cee3fd4e9c14242a6ed038d4d
Riya Dhariwal <rdhariwa@sfu.ca>
2020-03-09 19:21:55 -0700

    Merge branch '4-inspection-loader-class-21'

---
+++ app/src/main/java/com/carbon/restaurantinspection/model/Violation.java
@@ -0,0 +1,82 @@
+package com.carbon.restaurantinspection.model;
+
+import java.util.Hashtable;
+
+/*
+  Violation class models a violation found in an inspection report.
+  It contains the violation's code, status, description and type.
+ */
+public class Violation {
+    private String code;
+    private String status;
+    private String description;
+    private String type;
+
+    public Violation(String violation) {
+        String[] violationArray = violation.split(",");
+        code = violationArray[0];
+        status = violationArray[1];
+        description = violationArray[2] + "," + violationArray[3];
+    }
+
+    private void setTypeFromCode(String code) {
+        Hashtable<String, String> threeCode = new Hashtable<>();
+        threeCode.put("311", "Permit");
+        threeCode.put("312", "Permit");
+        threeCode.put("306", "Food");
+        threeCode.put("304", "Pest");
+        threeCode.put("305", "Pest");
+        threeCode.put("314", "Hygiene");
+        threeCode.put("311", "Permit");
+        threeCode.put("311", "Permit");
+        Hashtable<String, String> otherCode = new Hashtable<>();
+        otherCode.put("1", "Permit");
+        otherCode.put("2", "Food");
+        otherCode.put("4", "Hygiene");
+        otherCode.put("5", "Foodsafe");
+        if (code.substring(0, 1).equals("3")) {
+            if (threeCode.containsKey(code)) {
+                this.type = threeCode.get(code);
+            }
+            else {
+                this.type = "Equipment";
+            }
+        }
+        else {
+            this.type = otherCode.get(code.substring(0, 1));
+        }
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
