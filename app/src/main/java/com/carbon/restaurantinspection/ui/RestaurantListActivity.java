b9b4bb41004ef4e580719ab28e0513ca1e0959b3
Lina Su <linas@sfu.ca>
2020-03-09 20:25:17 -0700

    Add other icons to different restaurants.
    Create more icons to display for different restaurants in the restaurants list.

--- app/src/main/java/com/carbon/restaurantinspection/ui/RestaurantListActivity.java
+++ app/src/main/java/com/carbon/restaurantinspection/ui/RestaurantListActivity.java
@@ -15,21 +15,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.carbon.restaurantinspection.R;
+import com.carbon.restaurantinspection.model.InspectionDetail;
+import com.carbon.restaurantinspection.model.InspectionManager;
 import com.carbon.restaurantinspection.model.Restaurant;
 import com.carbon.restaurantinspection.model.RestaurantManager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RestaurantListActivity extends AppCompatActivity {
 
-    //ArrayList<String> name = new ArrayList<>();
+    ArrayList<Integer> numIssues = new ArrayList<Integer>();
 
     // calls RestaurantManager class
     private RestaurantManager restaurantManager;
-    //private Restaurant restaurant;
-
-    //restaurantsArray restaurant = new restaurantsArray();
-    //private ArrayList<restaurants> restList = restaurant.getRestaurantList();
+    private InspectionManager inspectionManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +38,19 @@ public class RestaurantListActivity extends AppCompatActivity {
 
         // Get that one instance that RestaurantManager class produced
         restaurantManager = RestaurantManager.getInstance(this);
-        //Restaurant restaurant = new Restaurant();
+        //inspectionManager = InspectionManager.getInstance(this);
 
         populateRestaurantListView();
     }
 
     private void populateRestaurantListView() {
-        //Log.d("Test", "testing");
-        //create an array list of restaurant names
-        /**for (Restaurant restaurants: restaurantManager) {
-            name.add(restaurants.getName());
-        }**/
 
         // Build adapter
-        //ArrayAdapter<Restaurant> adapter = new ArrayAdapter<Restaurant>(this,R.layout.display_restaurant_list_activity);
-
-
         ArrayAdapter<Restaurant> adapter = new MyListAdapter();
-        //ArrayAdapter<restaurant> adapter = new MyListAdapter();
-        //configure list view
 
+        //configure list view
         ListView list = (ListView) findViewById(R.id.restaurant_list_view);
         list.setAdapter(adapter);
-
     }
 
     private class MyListAdapter extends ArrayAdapter<Restaurant> {
@@ -75,11 +65,30 @@ public class RestaurantListActivity extends AppCompatActivity {
             }
 
             // find the restaurant
-            String currentRestaurants = restaurantManager.getRestaurant(position).getName();
+            String currentRestaurants = restaurantManager.getRestaurant(position).getName().replace("\"", "");
 
             //fill in the view
             ImageView imageView = (ImageView) itemView.findViewById(R.id.item_restaurant_icon);
-            imageView.setImageResource(R.drawable.a_w_restaurant_icon);
+            if (currentRestaurants.contains("A&W")){
+                imageView.setImageResource(R.drawable.a_w_restaurant_icon);
+            } else
+                imageView.setImageResource(R.drawable.beer_icon);
+
+            int report = 0;
+
+
+//           String restaurantTrackingNum = restaurantManager.getRestaurant(position).getTrackingNumber();
+//           ArrayList<InspectionDetail> inspections = inspectionManager.getInspections(restaurantTrackingNum);
+//
+//                TextView numIssuesText = (TextView) itemView.findViewById(R.id.num_issues_textview);
+//                numIssuesText.setText(inspections.size());
+
+
+            /*for (int i=0; i<inspections.size(); i++) {
+                    numIssues.add(inspectionDetails.getNumCritical());
+                    numIssues.add(inspectionDetails.getNumNonCritical());
+                }
+            }*/
 
             TextView restaurantNameText = (TextView) itemView.findViewById(R.id.restaurant_name_textview);
             restaurantNameText.setText(currentRestaurants);
