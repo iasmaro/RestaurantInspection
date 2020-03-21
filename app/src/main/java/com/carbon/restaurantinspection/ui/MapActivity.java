7e5c0ee601f51c2fe9e2494f942ded7aec5b324a
Lina Su <linas@sfu.ca>
2020-03-21 15:06:37 -0700

    Get current location on google maps.
    Display the user's current location on the map.

--- app/src/main/java/com/carbon/restaurantinspection/ui/MapActivity.java
+++ app/src/main/java/com/carbon/restaurantinspection/ui/MapActivity.java
@@ -1,30 +1,46 @@
 package com.carbon.restaurantinspection.ui;
 
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.carbon.restaurantinspection.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     private Boolean mLocationPermissionsGranted = false;
     private GoogleMap googleMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
@@ -77,9 +93,46 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
+//---------------------------------------------------------------------------------------------------------------------------
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+
+        if (mLocationPermissionsGranted == true) {
+            // when permission is granted, get the current location
+            getCurrentLocation();
+
+            // blue dot of current location 
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                    (this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+
+        }
+    }
+
+    private void getCurrentLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if(mLocationPermissionsGranted) {
+            Task location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete (@NonNull Task task) {
+                    if(task.isSuccessful()) {
+                        Location currentLocation = (Location) task.getResult();
+                        moveCamera (new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()), DEFAULT_ZOOM);
+//                        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("current location");
+//                        googleMap.addMarker(markerOptions);
+                    }
+                }
+            });
+        }
+    }
+    private void moveCamera(LatLng latLng, float zoom){
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
     }
 }
+
