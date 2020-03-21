c047bf4ee444ce774540b2785e71c683d721c1fd
Ivan <ivan_asmaro@sfu.ca>
2020-03-20 20:29:27 -0700

    Complete checkForUpdates.
    Check if current date is 20 hours after the latest data update
    date. Save latest data update date.

--- app/src/main/java/com/carbon/restaurantinspection/model/UpdateDownloader.java
+++ app/src/main/java/com/carbon/restaurantinspection/model/UpdateDownloader.java
@@ -2,23 +2,30 @@ package com.carbon.restaurantinspection.model;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
+import java.math.BigInteger;
 import java.util.Date;
 
 public class UpdateDownloader {
-    public static final String LAST_DOWNLOAD = "LastDownload";
-    public static final int MILLISECS_TO_HOURS = 3600000;
-    public boolean checkForUpdates(Context context) {
+    private static final String LAST_DOWNLOAD = "LastDownload";
+    private static final int MILLISECS_TO_HOURS = 3600000;
+    private static final BigInteger DEFAULT_DATE = new BigInteger("1584687607039");
+    private boolean checkForUpdates(Context context) {
         Date date = new Date();
         SharedPreferences prefs = context.getSharedPreferences(LAST_DOWNLOAD, Context.MODE_PRIVATE);
-        String savedDate = prefs.getString(LAST_DOWNLOAD, "19900101");
-        // return if date - savedDate > 24 hours
-        return false;
+        long savedDate = prefs.getLong(LAST_DOWNLOAD, DEFAULT_DATE.longValue());
+        long hours = (date.getTime() - savedDate)/MILLISECS_TO_HOURS;
+        return hours >= 20;
     }
     public boolean updatesAvailable(Context context) {
-        // check city of surrey website for updates
-        // return if date is less than saved date
-        return false;
+        boolean updatesAvailable = false;
+        if (checkForUpdates(context)) {
+            // check city of surrey website for updates
+            // return if date is less than saved date
+            updatesAvailable = true;
+        }
+        return updatesAvailable;
     }
 
     public void downloadUpdates(Context context) {
@@ -26,7 +33,10 @@ public class UpdateDownloader {
         // allow user to cancel if necessary
         // once download is complete, remove current data set
         // rename newly downloaded data set
-        // update savedDownloadDate
+        Date date = new Date();
+        SharedPreferences prefs = context.getSharedPreferences(LAST_DOWNLOAD, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong(LAST_DOWNLOAD, date.getTime());
     }
 
     public int downloadProgress() {
